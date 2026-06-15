6d807c72352b030391d8c220cb082658e7fd6ab4
Rohitpolkade <rohitpolkade325@gmail.com>
2026-06-15 14:08:26 +0530

    New Pattern Question Uploaded

---
+++ Pattern/HalfPyramid.java
@@ -0,0 +1,20 @@
+package Pattern;
+import java.util.Scanner;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int rows = sc.nextInt();
+
+        // Print half pyramid
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
